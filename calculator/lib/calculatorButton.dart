f527c76e74fe8ced4f8c48ece4357852ebe5c958
WidgetWiz <osamalala823@gmail.com>
2023-12-27 00:57:44 +0200

    Calculator ui.final logic?

--- calculator/lib/calculatorButton.dart
+++ calculator/lib/calculatorButton.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
-
+typedef buttonClick = void Function(String);
 class CalculatorButton extends StatelessWidget {
+
   final String title;
-  const CalculatorButton({super.key, required this.title});
+  buttonClick onButtonClick;
+    CalculatorButton({
+    super.key,
+    required this.title,
+    required this.onButtonClick
+  });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
         margin: EdgeInsets.only(top: 15.0, bottom: 15.0, left: 2.0, right: 2.0),
-        child: ElevatedButton(onPressed: () {},
+        child: ElevatedButton(onPressed: () {
+          onButtonClick(title);
+        },
           style: ElevatedButton.styleFrom(
             backgroundColor: Color(0xff808080), // Set the background color to aqua
             padding: EdgeInsets.all(16.0), // Adjust padding as needed
--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,39 +1,49 @@
 import 'package:calculator/calculatorButton.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
-  runApp(MySimpleApp());
+  runApp(CalcOUlator());
 }
 
-class MySimpleApp extends StatelessWidget {
+class CalcOUlator extends StatefulWidget{
+
+  @override
+  State<CalcOUlator> createState() {
+    return CalcOUlatorState();
+  }
+}
+
+class CalcOUlatorState extends State<CalcOUlator>{
+  String result = "";
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black54,
-        appBar: AppBar(
-          backgroundColor: Color(0xff252121),
-          title: Text('CalcOUlator',
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              color: Colors.white
+          backgroundColor: Colors.black54,
+          appBar: AppBar(
+            backgroundColor: Color(0xff252121),
+            title: Text("CalcOUlator",
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white
+              ),
             ),
           ),
-        ),
-        body: Column(
+          body: Column(
 
             children: [
               Expanded(
                 child: Container(
                   alignment: Alignment.centerLeft,
                   margin: EdgeInsets.symmetric(horizontal: 8),
-                  child: Text("9786754",
-                  style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                  ),
+                  child: Text(result,
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
               ), //Result
@@ -41,10 +51,10 @@ class MySimpleApp extends StatelessWidget {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    CalculatorButton(title: "C"),
-                    CalculatorButton(title: "DEL"),
-                    CalculatorButton(title: "%"),
-                    CalculatorButton(title: "/"),
+                    CalculatorButton(title: "C",onButtonClick: operatorClick,),
+                    CalculatorButton(title: "DEL",onButtonClick: operatorClick,),
+                    CalculatorButton(title: "%", onButtonClick: operatorClick,),
+                    CalculatorButton(title: "/", onButtonClick: operatorClick,),
                   ],
                 ),
               ),
@@ -52,10 +62,10 @@ class MySimpleApp extends StatelessWidget {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    CalculatorButton(title: "7"),
-                    CalculatorButton(title: "8"),
-                    CalculatorButton(title: "9"),
-                    CalculatorButton(title: "X"),
+                    CalculatorButton(title: "7",onButtonClick: myOnClick,),
+                    CalculatorButton(title: "8",onButtonClick: myOnClick,),
+                    CalculatorButton(title: "9",onButtonClick: myOnClick,),
+                    CalculatorButton(title: "X",onButtonClick: operatorClick,),
                   ],
                 ),
               ),
@@ -63,10 +73,10 @@ class MySimpleApp extends StatelessWidget {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    CalculatorButton(title: "4"),
-                    CalculatorButton(title: "5"),
-                    CalculatorButton(title: "6"),
-                    CalculatorButton(title: "-"),
+                    CalculatorButton(title: "4",onButtonClick: myOnClick,),
+                    CalculatorButton(title: "5",onButtonClick: myOnClick,),
+                    CalculatorButton(title: "6",onButtonClick: myOnClick,),
+                    CalculatorButton(title: "-",onButtonClick: operatorClick,),
                   ],
                 ),
               ),
@@ -74,10 +84,10 @@ class MySimpleApp extends StatelessWidget {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    CalculatorButton(title: "1"),
-                    CalculatorButton(title: "2"),
-                    CalculatorButton(title: "3"),
-                    CalculatorButton(title: "+"),
+                    CalculatorButton(title: "1",onButtonClick: myOnClick,),
+                    CalculatorButton(title: "2",onButtonClick: myOnClick,),
+                    CalculatorButton(title: "3",onButtonClick: myOnClick,),
+                    CalculatorButton(title: "+",onButtonClick: operatorClick,),
                   ],
                 ),
               ),
@@ -85,17 +95,92 @@ class MySimpleApp extends StatelessWidget {
                 child: Row(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    CalculatorButton(title: "0"),
-
-                    CalculatorButton(title: "."),
-                    CalculatorButton(title: "="),
+                    CalculatorButton(title: "0",onButtonClick: myOnClick,),
+                    CalculatorButton(title: ".",onButtonClick: myOnClick,),
+                    CalculatorButton(title: "=",onButtonClick: equalClick,),
                   ],
                 ),
               ),
 
             ],
-        )
+          )
       ),
     );
+
+  }
+
+  myOnClick(String title) {
+    print (title);
+    setState(() {
+      result+= title;
+    });
+
+  }
+
+  String savedNumber = "";
+  String savedOperator = "";
+
+  operatorClick(String operator) {
+    String savedNumber = "";
+    String savedOperator = "";
+
+      if(savedNumber.isEmpty){
+        savedNumber = result;
+        savedOperator = operator;
+        setState(() {
+          result = "";
+        });
+      }
+      else{
+        var lhs = savedNumber;
+        var rhs = result;
+        var res = calculate(lhs, operator, rhs);
+        savedNumber = res;
+
+        setState(() {
+          result= "";
+        });
+
+      }
+
+  }
+
+  equalClick(String operator){
+    var lhs = savedNumber;
+    var rhs = result;
+
+    var res = calculate(lhs, operator, rhs);
+
+    result = res;
+
+    setState(() {
+      savedNumber = "";
+      savedOperator = "";
+    });
+  }
+  String calculate(String lhs, String operator, String rhs) {
+    double n1 = double.parse(lhs);
+    double n2 = double.parse(rhs);
+    var res = 0.0;
+    if(operator == "+"){
+      res = n1 + n2;
+    }
+    else if(operator == "-"){
+      res = n1 - n2;
+    }
+    else if(operator == "X"){
+      res = n1 * n2;
+    }
+    else if(operator == "/"){
+      res = n1 / n2;
+    }
+    return "$res";
+
   }
 }
+
+
+
+
+
+
